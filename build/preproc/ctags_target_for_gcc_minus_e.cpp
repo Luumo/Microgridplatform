76d6b9587fc00949c42a21f9858b54221e76f9c9
Luumo <34240620+Luumo@users.noreply.github.com>
2019-12-12 16:07:47 +0100

    Line Protocol

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -38,12 +38,17 @@ double CurrentSensor::calcCurrentValue(){
 # 8 "c:\\Users\\LUMO\\Desktop\\Exjobb\\Software\\Microgridplatform\\main.ino" 2
 # 9 "c:\\Users\\LUMO\\Desktop\\Exjobb\\Software\\Microgridplatform\\main.ino" 2
 
+
+
+
 DHT OutdoorTempSensor(48, 11 /**< DHT TYPE 11 */);
 CurrentSensor Currentsensor1(A7);
 VoltageDivider voltagedivider1(A1, 1040000.0, 222500.0);
 RtdSensor RtdSensor1(A3);
 RainSensor Rainsensor1(52, 15);
 // WindSensor windsensor(A3);
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -52,33 +57,168 @@ void setup() {
 
 void loop() {
 
-  Serial.println("----- Outdoor Temp Sensor -----");
-  float h = OutdoorTempSensor.readHumidity();
-  float t = OutdoorTempSensor.readTemperature();
+  // line protocol: LOCATION,sensortype, timestamp, value
 
-  Serial.print("Temp: ");
-  Serial.println(t);
-  Serial.print("Humidity: ");
-  Serial.println(h);
 
-  Serial.println("----- Rain Sensor -----");
-  int rain = Rainsensor1.SenseRain();
-  int analog_rain = Rainsensor1.AnalogSignal();
-
-  Serial.print("Rain: ");
-  if (rain == 1){
-    Serial.println("Dry");
-    }
-    else{
-      Serial.println("Wet");
-    }
-  Serial.print("Analog signal: ");
-  Serial.println(analog_rain);
-
-  Serial.println("----- PT 1000 Sensor -----");
-  RtdSensor1.calcTemperature();
-
-  Currentsensor1.calcCurrentValue();
-  voltagedivider1.calculateVin();
+  Serial.print(1);
+  Serial.print(",");
+  Serial.print("OutdoorTempSensor");
+  Serial.print(",");
+  Serial.print(OutdoorTempSensor.readTemperature());
+  Serial.print("\n");
   delay(1000);
 }
+
+/*
+
+
+
+void loop() {
+
+
+
+  Serial.println("----- Outdoor Temp Sensor -----");
+
+  float h = OutdoorTempSensor.readHumidity();
+
+  float t = OutdoorTempSensor.readTemperature();
+
+
+
+  Serial.print("Temp: ");
+
+  Serial.println(t);
+
+  Serial.print("Humidity: ");
+
+  Serial.println(h);
+
+
+
+  Serial.println("----- Rain Sensor -----");
+
+  int rain = Rainsensor1.SenseRain();
+
+  int analog_rain = Rainsensor1.AnalogSignal();
+
+
+
+  Serial.print("Rain: ");
+
+  if (rain == 1){
+
+    Serial.println("Dry");
+
+    }
+
+    else{
+
+      Serial.println("Wet");
+
+    }
+
+  Serial.print("Analog signal: ");
+
+  Serial.println(analog_rain);
+
+
+
+  Serial.println("----- PT 1000 Sensor -----");
+
+  RtdSensor1.calcTemperature();
+
+
+
+  Currentsensor1.calcCurrentValue();
+
+  voltagedivider1.calculateVin();
+
+  delay(1000);
+
+}
+
+*/
+# 77 "c:\\Users\\LUMO\\Desktop\\Exjobb\\Software\\Microgridplatform\\main.ino"
+/*
+
+Schema for InfluxDX:
+
+See Line Protocol Syntax for influxDB
+
+
+
+data to be sent from arduino:
+
+clusterlocation,sensortype, value, date
+
+->
+
+this has to be parsed to python:
+
+data = [
+
+  {
+
+    "measurement": sensorcluster,
+
+    "tags": {
+
+      "location": location_var,
+
+      "sensortype": sensortype_Var
+
+    },
+
+    "time": iso,
+
+    "fields": {
+
+      "value": sensorvalue_Var
+
+    }
+
+  }
+
+]
+
+
+
+
+
+**SORTED BY SENSORCLUSTER**
+
+ex:
+
+
+
+INSERT sensorcluster,clusterlocation=1,sensortype=outdoortemp value=23 XXXXXXXXXXXX 
+
+see Milk-sensor documentation for futher info about data protocol from python
+
+
+
+name: Sensorcluster
+
+-----------------
+
+time                         sensortype       clusterlocation    value
+
+2019-12-11T19:30:00.xxxxxx   Humidity         1                    90
+
+2019-12-11T19:30:00.xxxxxx   outdoortemp      1                    22
+
+2019-12-11T19:30:00.xxxxxx   batterytemp      1                    23
+
+2019-12-11T19:30:00.xxxxxx   batteryCurrent   1                    8
+
+2019-12-11T19:30:00.xxxxxx   batteryvoltage   1                    12.3
+
+2019-12-11T19:30:00.xxxxxx   PVvoltage        1                    12.3
+
+2019-12-11T19:30:00.xxxxxx   PVCurrent        1                    8
+
+
+
+
+
+*/
--- build/sketch/Currentsensor.cpp.cpp
+++ build/sketch/Currentsensor.cpp.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
-#line 17 "c:\\Users\\LUMO\\Desktop\\Exjobb\\Software\\Microgridplatform\\main.ino"
-void setup();
 #line 22 "c:\\Users\\LUMO\\Desktop\\Exjobb\\Software\\Microgridplatform\\main.ino"
+void setup();
+#line 27 "c:\\Users\\LUMO\\Desktop\\Exjobb\\Software\\Microgridplatform\\main.ino"
 void loop();
 #line 0 "c:\\Users\\LUMO\\Desktop\\Exjobb\\Software\\Microgridplatform\\main.ino"
 #line 1 "c:\\Users\\LUMO\\Desktop\\Exjobb\\Software\\Microgridplatform\\Currentsensor.cpp"
@@ -44,6 +44,9 @@ double CurrentSensor::calcCurrentValue(){
 #include "RtdSensor.h"
 #include "RainSensor.h"
 
+
+#define LOCATION 1
+
 DHT OutdoorTempSensor(48, DHT11);
 CurrentSensor Currentsensor1(A7);
 VoltageDivider voltagedivider1(A1, 1040000.0, 222500.0);
@@ -51,10 +54,28 @@ RtdSensor RtdSensor1(A3);
 RainSensor Rainsensor1(52, 15);
 // WindSensor windsensor(A3);
 
+
+
 void setup() {          
   Serial.begin(115200);
   OutdoorTempSensor.begin();
 }
+
+void loop() {
+
+  // line protocol: LOCATION,sensortype, timestamp, value
+
+
+  Serial.print(LOCATION); 
+  Serial.print(",");
+  Serial.print("OutdoorTempSensor");
+  Serial.print(",");
+  Serial.print(OutdoorTempSensor.readTemperature());
+  Serial.print("\n");
+  delay(1000);
+}
+
+/*
 
 void loop() {
 
@@ -88,4 +109,47 @@ void loop() {
   voltagedivider1.calculateVin();
   delay(1000);
 }
+*/
 
+/*
+Schema for InfluxDX:
+See Line Protocol Syntax for influxDB
+
+data to be sent from arduino:
+clusterlocation,sensortype, value, date
+->
+this has to be parsed to python:
+data = [
+  {
+    "measurement": sensorcluster,
+    "tags": {
+      "location": location_var,
+      "sensortype": sensortype_Var
+    },
+    "time": iso,
+    "fields": {
+      "value": sensorvalue_Var
+    }
+  }
+]
+
+
+**SORTED BY SENSORCLUSTER**
+ex:
+
+INSERT sensorcluster,clusterlocation=1,sensortype=outdoortemp value=23 XXXXXXXXXXXX 
+see Milk-sensor documentation for futher info about data protocol from python
+
+name: Sensorcluster
+-----------------
+time                         sensortype       clusterlocation    value
+2019-12-11T19:30:00.xxxxxx   Humidity         1                    90
+2019-12-11T19:30:00.xxxxxx   outdoortemp      1                    22
+2019-12-11T19:30:00.xxxxxx   batterytemp      1                    23
+2019-12-11T19:30:00.xxxxxx   batteryCurrent   1                    8
+2019-12-11T19:30:00.xxxxxx   batteryvoltage   1                    12.3
+2019-12-11T19:30:00.xxxxxx   PVvoltage        1                    12.3
+2019-12-11T19:30:00.xxxxxx   PVCurrent        1                    8
+
+
+*/
