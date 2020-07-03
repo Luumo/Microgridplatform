fc727b1ef5d5af53d8e2c94c98285f9eed43b32b
Luan Mollakuqe <luan.mollakuqe@infotiv.se>
2020-07-04 00:30:15 +0200

    deleted build/

--- build/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-c:\Users\LUMO\Desktop\Exjobb\Software\Microgridplatform\build\core\hooks.c.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\hooks.c
--- build/sketch/Sampling.h
+++
@@ -1,8 +0,0 @@
-#ifndef SAMLING_H
-#define SAMPLING_H
-
-#include <arduino.h>
-
-float data_sampling(int data, int interval);
-
-#endif //SAMPLING_H
